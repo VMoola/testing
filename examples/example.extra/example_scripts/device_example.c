5b2b023ac0b21823db7df804fa8cbc23eba8468f
Vishal Moola (Oracle) <vishal.moola@gmail.com>
2025-10-08 06:44:02 -0700

    Examples: make module example scalable
    The old module example hard coded ioctls. Modify the code so that we can
    pass in an ioctl value, ioctl arguments, and a device name.
    
    We also improve upon the device example to showcase when to use which
    allocation type, and interpreting ioctl passed arguments.
    
    Signed-off-by: Vishal Moola (Oracle) <vishal.moola@gmail.com>

--- examples/example.extra/example_scripts/device_example.c
+++ examples/example.extra/example_scripts/device_example.c
@@ -1,38 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
+#include <linux/ioctl.h>
 #include <string.h>
 
-int main()
+// (type, nr, datatype)
+#define ALLOC _IOWR('a', 1, int)
+#define FREE _IOWR('a', 2, int)
+
+static int fd;
+
+static int mmap_example()
 {
+	//mmap example below
 	int len = 10000;
-	//assuming our character device was setup at /dev/[name] where name is hello
-	int fd = open("/dev/hello", O_RDWR, 0644); //calls open
-
-	if (fd == -1) {
-		printf("dead");
-		return -1;
-	}
-
-	char txt[] = "HO HO HO I'm santa";
-	//ioctl is : (fd, enum, args pointer)
-	int out = ioctl(fd, 115, txt); //calls ioctl
-
-	if (out < 0)
-		printf("something is wrong %d", out);
-	else
-		printf("theoretical success");
-
 	char *x = mmap(0, len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0); //mmap
-	if (!x) {
+	if (x == MAP_FAILED) {
 		printf("mmap dead");
 		return -1;
 	}
 
 	munmap(x, len); //no munmap definition, we have to fold it into release
+	return 0;
+}
 
+int main(int argc, char *argv[])
+{
+	if (argc == 1) {
+		printf("No device specified\n");
+		return -1;
+	}
+
+	char *device = argv[1];
+	unsigned int io = 0;
+	if (argv[2] != NULL) {
+		io = atoi(argv[2]);
+		switch (io) {
+			case 1:
+				io = ALLOC;
+				break;
+			case 2:
+				io = FREE;
+				break;
+		};
+	}
+	char *args = NULL;
+	if (argc > 3)
+		args = argv[3];
+
+	printf("number of args %d\ndevice: %s\nioctl: %d\nargs: %s\n", argc, device, io, args);
+
+	//assuming our character device was setup at /dev/[name] where name is hello
+	fd = open(device, O_RDWR, 0644); //calls open
+
+	if (fd == -1) {
+		printf("Failed to open device!\n");
+		return -1;
+	}
+
+	//ioctl is : (fd, enum, args pointer)
+	if (io) {
+		int val = atoi(args); //assuming int for example
+		int out = ioctl(fd, io, &val); //calls ioctl
+
+		//Since RW, our ioctl also writes to this variable
+		printf("Output value from ioctl %d\n", val);
+	}
+
+	//mmap_example();
 	close(fd); //calls release
 	return 0;
 }
