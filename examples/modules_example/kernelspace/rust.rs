8c7da7a4d47f42cc5ce0c57511eeb4d74f5d3e37
Vishal Moola <vishal.moola@gmail.com>
2026-05-19 18:53:54 +0100

    modules_example: showcase a page in rust
    Use the basic Page type. Rust has implemented a Page type. Iterate on
    our example module to showcase how to use it.
    
    This also shows us error handling in rust. For that reason, we also
    added the C debugging blocks.
    
    Signed-off-by: Vishal Moola <vishal.moola@gmail.com>

--- examples/modules_example/kernelspace/rust.rs
+++ examples/modules_example/kernelspace/rust.rs
@@ -1,6 +1,7 @@
 #![allow(missing_docs)]
 
 use kernel::prelude::*;
+use kernel::page::Page;
 
 module! {
     type: Hello,
@@ -15,6 +16,16 @@ struct Hello;
 impl kernel::Module for Hello {
     fn init(_module: &'static kernel::ThisModule) -> kernel::error::Result<Self> {
         pr_info!("Hello from Rust kernel module!\n");
+        let _x = hi();
+        let x: Result<u32, _> = Err(0);
+        if x.is_ok() {
+            // This path won't happen because we've hardset x to fail
+            pr_info!("Its done! {:?} \n", x.unwrap());
+        } else {
+            // Using :? lets us skip the unwrap()
+            // For debugging, we should always do this to dump the whole type
+            pr_info!("{:?}\n", x);
+        }
         Ok(Self)
     }
 }
@@ -23,4 +34,21 @@ impl Drop for Hello {
     fn drop(&mut self) {
         pr_info!("Goodbye world\n");
     }
+}
+
+#[inline(never)]
+fn hi() -> kernel::error::Result<u32>{
+    let x = 10;
+    let page = Page::alloc_page(GFP_KERNEL)?;
+
+    //We should always print these using the .as_ptr() values
+    pr_emerg!("This simply prints the address {:?}", page.as_ptr());
+    //unsafe { kernel::bindings::dump_page(page.as_ptr(), "example".as_ptr()) };
+    //unsafe { kernel::bindings::dump_stack() };
+    //unsafe { kernel::bindings::WARN_ON(true) };
+    drop(page);
+
+    //The point of the Ok is to return the expected value of the function
+    //If we fail earlier, Error populates and lets the caller handle it
+    Ok(x)
 }
