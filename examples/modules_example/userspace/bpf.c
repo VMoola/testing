996bc49a1110b8cadc08463c99d49d13164da572
Vishal Moola <vishal.moola@gmail.com>
2026-07-25 15:19:27 -0700

    examples: improve BPF example
    Update the example to make use of the BPF_PROG macro.
    
    BPF is written with support for many abstractions and helpers. One big
    helper is the BPF_PROG macro for extracting arguments from tracepoints.
    This makes writing these functions clear and sensible, compared to
    manually extracting the arguments every time.
    
    Signed-off-by: Vishal Moola <vishal.moola@gmail.com>

--- examples/modules_example/userspace/bpf.c
+++ examples/modules_example/userspace/bpf.c
@@ -3,16 +3,16 @@
 
 // BPF definitions
 #include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
 
 // Map the program to the intercept point - See bpf SEC
 SEC("raw_tp/sys_enter")
-int helloworld(struct bpf_raw_tracepoint_args *ctx)
+int BPF_PROG(helloworld, struct pt_regs *regs, long syscall)
 {
 	/*
 	 * TP_PROTO defines what we can grab from ctx->args
-	 * I.e. for sys_enter, we have pt_regs, id.
+	 * BPF_PROG does this for us in a clean readable way.
 	 */
-	unsigned long syscall = ctx->args[1];
 
 	// See arch/[arch]/entry/syscalls/syscall.tbl for ids
 	if (syscall != 9) {
